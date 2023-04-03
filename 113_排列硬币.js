e5297098a76b0fbd47ef01e9ef22efcd65bedc09
Ali <ali_ovo@qq.com>
2023-04-03 11:27:27 +0800

    add 113

--- 113_排列硬币.js
+++ 113_排列硬币.js
@@ -1,4 +1,13 @@
 var arrangeCoins = function (n) {
+  // let total = 0;
+  // let res = 0;
+  // for (let index = 0; index < n; index++) {
+  //   total += index + 1;
+  //   if (total > n) break;
+  //   res++
+  // }
+  // return res;
+
   let left = 1,
     right = n
   while (left < right) {
