2f5cad54d818f2f399fdde05533c758f0fdbf89e
Ali <ali_ovo@qq.com>
2023-04-10 11:00:32 +0800

    add 117

---
+++ 117_构造矩形.js
@@ -0,0 +1,9 @@
+var constructRectangle = function (area) {
+  let w = Math.floor(Math.sqrt(area))
+  while (area % w !== 0) {
+    --w
+  }
+  return [Math.floor(area / w), w]
+}
+
+constructRectangle(122122) // [427, 286]
