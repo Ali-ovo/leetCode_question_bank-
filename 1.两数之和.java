df6be68cf34de39b43023519ba027e060205aeaa
Ali <ali_ovo@qq.com>
2023-12-12 16:53:06 +0800

    init java leetcode

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -5,9 +5,14 @@
  */
 
 // @lc code=start
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-         Map<Integer, Integer> numMap = new HashMap<>();
+        Map<Integer, Integer> numMap = new HashMap<>();
         int n = nums.length;
 
         // Build the hash table
@@ -19,12 +24,21 @@ class Solution {
         for (int i = 0; i < n; i++) {
             int complement = target - nums[i];
             if (numMap.containsKey(complement) && numMap.get(complement) != i) {
-                return new int[]{i, numMap.get(complement)};
+                return new int[] { i, numMap.get(complement) };
             }
         }
 
-        return new int[]{}; // No solution found
+        return new int[] {}; // No solution found
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = s.twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
     }
 }
 // @lc code=end
-
