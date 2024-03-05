e7b7787d73d69883c44d9616aa9d7b79a36e2a91
Ali <ali_ovo@qq.com>
2024-03-05 10:45:08 +0800

    add 19

--- 17.只出现一次的数字.js
+++
@@ -1,9 +0,0 @@
-var singleNumber = function (nums) {
-  return nums.reduce((a, b) => {
-
-    return a ^ b
-  })
-};
-
-// 异或方法 1 ^ 1 = 0   2 ^ 2 = 0  0 ^ 4 = 4
-console.log(singleNumber([4, 1, 2, 1, 2]));
