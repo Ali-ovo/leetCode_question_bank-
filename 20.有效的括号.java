bd31392fae690be4099bf1b3aa30b35c620c4601
Ali <ali_ovo@qq.com>
2024-03-06 10:47:26 +0800

    add 20

---
+++ 20.有效的括号.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=java
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+
+    }
+}
+// @lc code=end
---
+++ 20.有效的括号.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  const stack = []
+  const map = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]]) {
+      stack.push(s[i])
+    } else {
+      if (s[i] !== map[stack.pop()]) {
+        return false
+      }
+    }
+  }
+
+  return stack.length === 0
+}
+// @lc code=end
